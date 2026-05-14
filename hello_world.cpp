b5fa10072ea57bc7fa9427d1ec7fd23a5abbd61d
marykos0505 <TheKittenMay@yandex.ru>
2026-05-14 08:12:33 +0000

    updated hello_world with username input

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,3 +1,4 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main() { cout << "Hello world" << endl; return 0; }
+int main() { string name; cin >> name; cout << "Hello world from @" << name << endl; return 0; }
