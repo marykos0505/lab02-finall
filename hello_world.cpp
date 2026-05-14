5ef7b2cc33115ccc260126b142029aef6f966417
marykos0505 <TheKittenMay@yandex.ru>
2026-05-14 13:15:26 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     std::string name;
-    std::cout << "Enter your name: ";
+    std::cout << "Enter your name::: ";
     std::cin >> name;
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
